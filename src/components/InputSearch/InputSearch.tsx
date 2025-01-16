4d14f2c981a56d24a4b2aebe6c0b3d1e9255532a
Oliwier <worekit@gmail.com>
2025-01-16 15:25:01 +0100

    searching new friends

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -3,14 +3,20 @@ import "./style.scss";
 
 type InputProps = {
   width: string;
+  onChange: (prefix: string) => void;
 };
 
-export function InputSearch({ width }: InputProps) {
+export function InputSearch({ width, onChange }: InputProps) {
   return (
     <>
       <div className="search_container">
         <img src={glass} alt="magnifying-glass" />
-        <input style={{ width: width }} className="searchbar" placeholder="Search" />
+        <input
+          style={{ width: width }}
+          className="searchbar"
+          placeholder="Search"
+          onChange={(e) => onChange(e.target.value)}
+        />
       </div>
     </>
   );
--- src/components/SearchFriendDialog/SearchFriendDialog.tsx
+++ src/components/SearchFriendDialog/SearchFriendDialog.tsx
@@ -1,15 +1,40 @@
 import "./style.scss";
 
+import { db } from "../../lib/firebase";
+import { query, where, collection, getDocs } from "firebase/firestore";
+
 import { InputSearch } from "../InputSearch/InputSearch";
 import { FriendInfo } from "../FriendInfo/FriendInfo";
 import { AddButton } from "../AddButton/AddButton";
+import { useState } from "react";
+import { useUserStore } from "../../lib/userStore";
 
 type SearchFriendDialogProps = {
   handleDisplayAddFriend: () => void;
 };
 
 export function SearchFriendDialog({ handleDisplayAddFriend }: SearchFriendDialogProps) {
-  const users: string[] = [];
+  const { currentUser } = useUserStore();
+  const [users, setUsers] = useState<string[]>([]);
+
+  async function searchUsersByUsername(prefix: string) {
+    if (prefix !== "") {
+      const usersRef = collection(db, "users");
+      const q = query(
+        usersRef,
+        where("username", ">=", prefix),
+        where("username", "<=", prefix + "\uf8ff")
+      );
+
+      const querySnapshot = await getDocs(q);
+
+      const results: string[] = querySnapshot.docs
+        .map((doc) => doc.data().username)
+        .filter((username) => username != currentUser?.username);
+
+      setUsers(() => results);
+    }
+  }
 
   return (
     <>
@@ -17,7 +42,7 @@ export function SearchFriendDialog({ handleDisplayAddFriend }: SearchFriendDialo
         <div className="dialog_overlay">
           <div className="dialog">
             <div className="search_container">
-              <InputSearch width="80%" />
+              <InputSearch width="80%" onChange={searchUsersByUsername} />
               <div className="exit" onClick={handleDisplayAddFriend}>
                 X
               </div>
